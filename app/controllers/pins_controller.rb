d50eaf1114116d29a89f9988615cd315eb81f0ab
Neniah <maria.lobillo.santos@gmail.com>
2015-06-19 20:28:44 +0200

    built pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,12 @@
 class PinsController < ApplicationController
 
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
   def index
+
+  end
+
+  def show
 
   end
 
@@ -10,11 +16,21 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "Successfully created new Pin"
+    else
+      render 'new'
+    end
   end
 
   private
 
     def pin_params
       params.require(:pin).permit(:title, :description)
+    end
+
+    def find_pin
+      @pin = Pin.find(params[:id])
     end
 end
